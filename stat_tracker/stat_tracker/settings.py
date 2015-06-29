f5c0f6078be9632e54034e5b6d031d99d0b82f6c
John Waldrep <johnwaldrep@gmail.com>
2015-06-29 15:57:58 -0400

    Add pagination

--- stat_tracker/stat_tracker/settings.py
+++ stat_tracker/stat_tracker/settings.py
@@ -108,3 +108,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
