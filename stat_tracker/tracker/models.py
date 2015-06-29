bd48dbe5c2cf2f54d8cd379e867d2a493b3086b4
John Waldrep <johnwaldrep@gmail.com>
2015-06-29 15:57:46 -0400

    Update models and migrations

--- stat_tracker/tracker/models.py
+++ stat_tracker/tracker/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+# from django.utils import timezone
+from datetime import datetime
+
+"""
+Activity:
+    id
+    title
+Stat:
+    id
+    date
+    value
+"""
+
 
 class Activity(models.Model):
-    pass
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
 
 class Stat(models.Model):
-    pass
+    date = models.DateField(default=datetime.today)
+    activity = models.ForeignKey(Activity)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return '{}:{} @ {}'.format(self.activity, self.value, self.date)
