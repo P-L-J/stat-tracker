aa015c90cd3402b635194de571defdcd47c3b69d
Peter McNeary <plmcneary@icloud.com>
2015-06-30 14:09:30 -0400

    Adding templating for activities page

--- stat_tracker/static/js/controllers/activities_list.js
+++ stat_tracker/static/js/controllers/activities_list.js
@@ -1,5 +1,15 @@
 app.router.route('activities', function () {
   console.log("Activities Page");
 
-  app.show('activities-list');
+  $.ajax({
+    url: '/api/activities',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      app.show('activities-list', data);
+    }
+  });
+
+  ;
 });
--- stat_tracker/static/js/controllers/login.js
+++ stat_tracker/static/js/controllers/login.js
@@ -2,4 +2,12 @@ app.router.route('', 'login', function () {
   console.log("Login Page");
 
   app.show('login');
+
+  $(".login-button").on('click', function() {
+    app.goto('activities');
+  });
+
+  $(".register-button").on('click', function() {
+    app.goto('registration');
+  });
 });
